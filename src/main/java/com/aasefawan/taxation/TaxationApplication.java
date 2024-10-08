0f588dcb87afb67f961b0a6adf9a59253dfa573b
technicalwajahat <wajahat.awan.924@gmail.com>
2024-10-08 17:10:41 +0500

    initial taxation project

---
+++ src/main/java/com/aasefawan/taxation/TaxationApplication.java
@@ -0,0 +1,13 @@
+package com.aasefawan.taxation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aasefawan/taxation/TaxationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aasefawan.taxation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
